cb0d7c32843da335c35b2e88056774e103144f04
BrainJarProductions <vgrilli1@gmail.com>
2022-02-13 09:18:35 -0500

    first late commit...

---
+++ src/pages/pizzas.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PizzasPage() {
+  return (
+    <>
+      <p>Hey! Im the Pizzas Page</p>
+    </>
+  );
+}
---
+++ src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SliceMasters() {
+  return (
+    <>
+      <p>Hey! Im the Slice Masters Pages</p>
+    </>
+  );
+}
