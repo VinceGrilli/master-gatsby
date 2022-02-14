b62ecd06fb8c73a7725aa93a846ec7117f732654
BrainJarProductions <vgrilli1@gmail.com>
2022-02-14 13:32:53 -0500

    set up beers api

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import fetch from 'isomorphic-fetch';
 
 async function turnPizzasIntoPages({ graphql, actions }) {
   // get a template for this page
@@ -56,8 +57,41 @@ async function turnToppingsIntoPages({ graphql, actions }) {
         // TODO Regex for topping
       },
     });
-    console.log(topping.name);
   });
+}
+// Using an externail api and putting into gatsby
+async function fetchBeersAndTurnIntoNodes({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) {
+  // fetch a list of beers
+  const res = await fetch('https://api.sampleapis.com/beers/ale');
+  const beers = await res.json();
+  // loop over each one
+  for (const beer of beers) {
+    // create a node for each beer
+    const nodeMeta = {
+      id: createNodeId(`beer-${beer.name}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Beer',
+        mediaType: 'application/json',
+        contentDigest: createContentDigest(beer),
+      },
+    };
+    // create a node for that beer
+    actions.createNode({
+      ...beer,
+      ...nodeMeta,
+    });
+  }
+}
+
+export async function sourceNodes(params) {
+  // fetch a list of beers and source them into our gatsby API
+  await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
 }
 
 export async function createPages(params) {
